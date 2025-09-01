754e200930e374470fb15828dd978a4ca3a58fd1
JHanses <hanselberger@me.com>
2025-09-01 14:04:05 -0500

    Add arrays.js with full content

--- arrays.js
+++ arrays.js
@@ -1,3 +1,32 @@
-arrays
+//building and creating arrays practice //
+
+// Task 1: Create Arrays//
+
+let groceryItems = ["apples", "oranges", "milk", "eggs", "bread"]; // 5 grocery items
+let prices = [10.99, 5.03, 14.78]; // 3 prices
+let balloonColors = ["black", "white", "pink", "periwinkle"]; // third array
+
+// Task 2: Access and Log the Values //
+
+// log the entire array of grocery items//
+
+console.log(groceryItems);
+
+// log the entire array of prices//
+
+console.log(prices);
+
+// log three indivdually chosen items //
+
+console.log(groceryItems[0]); // "apples"
+console.log(groceryItems[1]); // "oranges"
+console.log(groceryItems[2]); // "milk"
+
+// log two prices of my choice //
+console.log(prices[0]); // 10.99
+console.log(prices[2]); //14.78
+
+
+
 
 
